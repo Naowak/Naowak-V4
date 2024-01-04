747806fd73efb622ac607330455ae8b15b1a8314
Naowak <yannis.bendiouis@gmail.com>
2024-01-04 13:07:43 +0100

    add markdown to first articles, remove api routes, use public folder instead

--- app/page.js
+++ app/page.js
@@ -1,11 +1,26 @@
-"use client";
-
 import Image from 'next/image'
+import fs from 'fs';
+import path from 'path';
+import Markdown from '@/components/Markdown';
 
-export default function Home() {
+async function readMarkdownFile() {
+  const filePath = './public/articles/alakazam/markdown.txt'
+  try {
+    const data = await fs.promises.readFile(filePath, 'utf8');
+    return data;
+  } catch (err) {
+    console.error(`Error reading file from disk: ${err}`);
+  }
+}
+
+export default async function Home() {
+
+  const content = await readMarkdownFile()
+  console.log(content)
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <Markdown>{content}</Markdown>
     </main>
   )
 }
---
+++ components/Markdown.js
@@ -0,0 +1,119 @@
+import ReactMarkdown from 'react-markdown'
+import { BiSolidQuoteAltLeft, BiSolidQuoteAltRight } from 'react-icons/bi'
+
+export default function Markdown ({ children }) {
+
+  const components = {
+    h1: H1,
+    h2: H2,
+    h3: H3,
+    h4: H4,
+    h5: H5, // Signature bottom right
+    img: Media,
+    li: Line,
+    p: Paragraph,
+    a: Anchor,
+    blockquote: Quote,
+    code: Code,
+  }
+
+  return (
+    <ReactMarkdown
+      components={components}
+      className='flex flex-col gap-4 w-full text-base'
+    >
+      {children}
+    </ReactMarkdown>
+  )
+}
+
+function H1 (props) {
+  return (
+    <h1 className="text-3xl font-bold mb-2" {...props} />
+  )
+}
+
+function H2 (props) {
+  return (
+    <h1 className="text-2xl font-bold mb-2" {...props} />
+  )
+}
+
+function H3 (props) {
+  return (
+    <h1 className="text-xl font-bold mb-2" {...props} />
+  )
+}
+
+function H4 (props) {
+  return (
+    <h1 className="text-lg font-bold mb-2" {...props} />
+  )
+}
+
+function H5 (props) {
+  return (
+    <h1 className="text-lg font-bold text-right mt-4" {...props} />
+  )
+}
+
+function Media (props) {
+  return (
+    <span className="flex flex-col items-center gap-4 my-4 text-md text-gray-400">
+      <img {...props} />
+      {props.alt}
+    </span>
+  )
+}
+
+function Line (props) {
+  if (!props.ordered) {
+    return <span className="mb-2 text-primary">• {props.children}<br/></span>
+  }
+  return (
+    <span className='flex flex-row gap-2 pl-4 items-start'>
+      <h1 className="text-primary font-semibold">{`${props.index+1}.`}</h1>
+      <h1 className="text-primary -mt-2">{props.children}</h1>
+    </span>
+  )
+}
+
+
+function Paragraph (props) {
+  return (
+    <p className="w-full my-2 text-md text-start" {...props} />
+  )
+}
+
+function Anchor (props) {
+  return (
+    <a className="text-red-800 underline underline-offset-2" {...props} />
+  )
+}
+
+function Quote (props) {
+  return (
+    <div className='bg-gray-200 rounded-md py-1'>
+      <BiSolidQuoteAltLeft color='gray' className='ml-1'/>
+        <h1 className="pl-2 italic text-gray-800" {...props} />
+      <BiSolidQuoteAltRight color='gray' className='ml-1'/>
+    </div>
+  )
+}
+
+function Code (props) {
+  if (props.inline) {
+    return (
+      <span className='bg-gray-100 font-code rounded break-all p-1 rounded'>
+        {props.children}
+      </span>
+    )
+  }
+  return (
+    <div className='bg-gray-100 font-code overflow-x-scroll p-4 pb-0 rounded text-start'>
+      {props.children[0].split('\n').map((line, i) => (
+        <span key={i} className='break-all'>{line}<br/></span>
+      ))}
+    </div>
+  )
+}

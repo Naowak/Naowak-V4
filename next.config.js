706b1a13977f3d65b3bfd4778725e07873b8d735
Naowak <yannis.bendiouis@gmail.com>
2024-03-16 14:30:33 +0100

    again

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    assetPrefix: process.env.NODE_ENV === 'development' ? "/" : "/Naowak-V4/",
 }
 
 module.exports = nextConfig;

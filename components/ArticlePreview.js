e7975fe0f19a463761bf42784614979074c21fd1
Naowak <yannis.bendiouis@gmail.com>
2024-03-16 14:16:32 +0100

    article preview

--- components/ArticlePreview.js
+++ components/ArticlePreview.js
@@ -7,7 +7,7 @@ export default function ArticlePreview({ article }) {
   return (
     <div className="flex flex-row p-4 gap-8">
       <div className="w-3/5 h-60 overflow-hidden rounded-xl shadow">
-        <Link href={`/article/${article.id}`}>
+        <Link href={`/Naowak-V4/article/${article.id}`}>
           <img 
             src={article.preview} 
             alt="Article preview" 
@@ -16,7 +16,7 @@ export default function ArticlePreview({ article }) {
         </Link>
       </div>
       <div className='w-full'>
-        <Link href={`/article/${article.id}`}>
+        <Link href={`/Naowak-V4/article/${article.id}`}>
           <h2 className="text-2xl font-semibold mb-2">{article.title}</h2>
         </Link>
         <p className="text-sm text-gray-500">{article.date}</p>

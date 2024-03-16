c9b7382bb1216c7446da6da21e3c0e0fb660a64b
Naowak <yannis.bendiouis@gmail.com>
2024-03-16 13:04:45 +0100

    attempt basepath

--- libs/article.js
+++ libs/article.js
@@ -47,7 +47,7 @@ export async function loadPreviews() {
     const configPath = path.join(articlesDir, dir, 'config.json');
 
     const markdown = await fs.promises.readFile(markdownPath, 'utf8');
-    const preview = `./Naowak-V4/${path.relative('./public', previewPath)}`;
+    const preview = `/${path.relative('./public', previewPath)}`;
     const config = JSON.parse(await fs.promises.readFile(configPath, 'utf8'));
 
     return { 

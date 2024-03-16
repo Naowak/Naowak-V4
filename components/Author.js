16b44a621962d1142150c2872bbc279222957b64
Naowak <yannis.bendiouis@gmail.com>
2024-03-16 13:56:41 +0100

    update workflow (remove xport) and update images basepath

--- components/Author.js
+++ components/Author.js
@@ -1,8 +1,11 @@
 export default function Author () {
+
+  const basePath = process.env.NODE_ENV === 'development' ? "/" : "/Naowak-V4/";
+
   return (
     <div className='xl:fixed xl:w-1/5 xl:p-10'>
       <div className='bg-white rounded-xl overflow-hidden shadow-2xl'>
-        <img src='/picture.png' alt='user_picture' className='' />
+        <img src={`${basePath}picture.png`} alt='user_picture' className='' />
         <div className='p-2'>
           <h1 className='text-lg text-center font-semibold'>Yannis Bendi-Ouis</h1>
           <h2 className='text-sm text-center text-slate-500  p-0.5'>Doctorant en Intelligence Artificielle</h2>

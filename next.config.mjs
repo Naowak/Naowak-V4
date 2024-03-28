d7e7a6a5732e019bc7fc3d728619df79bf93ead5
Naowak <yannis.bendiouis@gmail.com>
2024-03-28 19:47:57 +0100

    update attempt basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    basePath: process.env.NODE_ENV === 'development' ? "" : "/Naowak-V4",
+    basePath: process.env.NODE_ENV === 'development' ? "" : "/Naowak-V4/",
     assetPrefix: process.env.NODE_ENV === 'development' ? "" : "/Naowak-V4/",
     images: {
         unoptimized: true,

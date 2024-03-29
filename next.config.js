bf1b2731c770f35356e0caffdceca2b8b11b002f
Naowak <yannis.bendiouis@gmail.com>
2024-03-29 13:25:22 +0100

    attempt to indicate website to assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    assetPrefix: process.env.NODE_ENV === 'development' ? "" : "https://github.naowak.fr/",
     output: "export",
 }
 

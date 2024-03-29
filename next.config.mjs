f3245b0ef47ef9d6fe91cfd314312c5901386407
Naowak <yannis.bendiouis@gmail.com>
2024-03-29 13:47:50 +0100

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,10 @@
 
 const nextConfig = {
     //assetPrefix: process.env.NODE_ENV === 'development' ? "" : "/Naowak-V4/",
-    basePath: '/',
-    output: "export",
-    images: {
-        unoptimized: true,
-    },
+    //basePath: '/',
+    //output: "export",
+    // images: {
+    //     unoptimized: true,
+    // },
 }
 export default nextConfig;

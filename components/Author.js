f7fd5b875dd0d1a71e4a51ff30463f10ef91fa82
Naowak <yannis.bendiouis@gmail.com>
2025-03-28 18:56:12 +0100

    update pic

--- components/Author.js
+++ components/Author.js
@@ -6,7 +6,7 @@ export default function Author () {
       <div className='p-4 md:p-8 xl:fixed xl:w-1/5 xl:p-10'>
         <div className='bg-white rounded-xl overflow-hidden shadow-2xl flex flex-col content-center'>
           <Link href={'/'}>
-            <img src={`/picture.png`} alt='user_picture' className='w-full' />
+            <img src={'/pic.png'} alt='user_picture' className='w-full' />
           </Link>
           <div className='p-2'>
             <h1 className='text-lg text-center font-semibold'>Yannis Bendi-Ouis</h1>
